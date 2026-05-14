c24b29266af711e0d9d02a20f50e40ac82aed137
mnmrukshan <mnmrukshan22@gmail.com>
2026-05-14 21:26:33 +0530

    completed app routing and integrated login functionality

---
+++ frontend/src/components/AppDownload/AppDownload.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './AppDownload.css'
+import { assets } from '../../assets/assets'
+
+const AppDownload = () => {
+    return (
+        <div className='app-download' id='app-download'>
+            <h1>Experience Tasto on Your Mobile</h1>
+            <p>For a faster and more seamless ordering experience, download our app today.</p>
+            <div className="app-download-platforms">
+                <img src={assets.play_store} alt="" />
+                <img src={assets.app_store} alt="" />
+            </div>
+        </div>
+    )
+}
+
+export default AppDownload
