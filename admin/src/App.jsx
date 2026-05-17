a06c94c090649b23405432475a014aeb06241cd7
mnmrukshan <mnmrukshan22@gmail.com>
2026-05-17 17:27:35 +0530

    Pass API URL to dashboard component inside Admin router

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -29,7 +29,7 @@ const App = () => {
           <Route path="/add" element={<Add url={url}/>} />
           <Route path="/list" element={<List url={url}/>} />
           <Route path="/orders" element={<Orders url={url}/>} />
-          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/dashboard" element={<Dashboard url={url}/>} />
         </Routes>
       </div>
     </div>
